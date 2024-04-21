6d8e208a52f16ce9f295e12ddedd4b06c10f17df
xfarkasp <xfarkasp@stuba.sk>
2024-04-21 11:24:02 +0200

    minor fixes to location trigger to only trigger if exemplar is not in transit

--- location_trigger.sql
+++ location_trigger.sql
@@ -1,18 +1,22 @@
 CREATE OR REPLACE FUNCTION check_exemplar_exposition()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM room r
-        JOIN location_table l ON r.id_museum = l.id
-        JOIN exposition ex ON r.id_exposition = ex.id
-        WHERE ex.id = NEW.id_exposition AND l.id != NEW.id_location
-    ) THEN
-        RAISE EXCEPTION 'Cannot update exemplar to an exposition in a different museum';
+    IF NEW.id_transit IS NULL THEN
+        -- Check if the new exposition is in the same museum's location
+        IF EXISTS (
+            SELECT 1
+            FROM room r
+            JOIN location_table l ON r.id_museum = l.id
+            JOIN exposition ex ON r.id_exposition = ex.id
+            WHERE ex.id = NEW.id_exposition AND l.id != NEW.id_location
+        ) THEN
+            RAISE EXCEPTION 'Cannot set exemplar to an exposition that is not in this museum!';
+        END IF;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
 
 CREATE TRIGGER prevent_exemplar_exposition_change
 BEFORE UPDATE ON exemplar
