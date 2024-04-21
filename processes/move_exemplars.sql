4f0c3fbb0119e54110e61e7d3c55024f04e7871d
xfarkasp <xfarkasp@stuba.sk>
2024-04-21 12:41:21 +0200

    move exemplar and trigger when trying to assign a room to a exemplar which is not in the same museum

---
+++ processes/move_exemplars.sql
@@ -0,0 +1,43 @@
+-- move the Mona Lisa to Salle des Caryatides
+UPDATE exemplar
+SET id_room = (
+    SELECT room.id
+    FROM room
+    WHERE room.name = 'Salle des Caryatides'
+)
+WHERE name = 'Mona Lisa';
+-- move the Mona Lisa to  Salle des États
+UPDATE exemplar
+SET id_room = (
+    SELECT room.id
+    FROM room
+    WHERE room.name = 'Salle des États'
+)
+WHERE name = 'Mona Lisa';
+
+-- try to move the Mona Lisa to Sala di Dante which is not located at the Louvre
+-- this will fail, because the trigger which checks if room location is the same as the exemplar location
+UPDATE exemplar
+SET id_room = (
+    SELECT room.id
+    FROM room
+    WHERE room.name = 'Sala di Dante'
+)
+WHERE name = 'Mona Lisa';
+
+SELECT
+    e.name,
+    e.validation_time,
+    condt.current_condition,
+    ot.owner_name,
+    cat.category_name,
+    loct.location_institute_name,
+    COALESCE(rt.name, 'Not yet displayed') AS room_name
+
+FROM exemplar as e
+    LEFT JOIN condition_table condt on e.id_condition = condt.id
+    LEFT JOIN owner_table ot on e.id_owner = ot.id
+    LEFT JOIN category_table cat on e.id_category = cat.id
+    LEFT JOIN location_table loct on e.id_location = loct.id
+    LEFT JOIN room rt on e.id_room = rt.id
+;
---
+++ triggers/room_location_trigger.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION check_exemplar_room_museum()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM room r
+        JOIN location_table l ON r.id_museum = l.id
+        WHERE r.id = NEW.id_room AND l.id != NEW.id_location
+    ) THEN
+        RAISE EXCEPTION 'Cannot assign room from a different museum to the exemplar!';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create a trigger to enforce the check on room museum before updating exemplar
+CREATE TRIGGER prevent_room_museum_mismatch
+BEFORE UPDATE ON exemplar
+FOR EACH ROW
+EXECUTE FUNCTION check_exemplar_room_museum();
